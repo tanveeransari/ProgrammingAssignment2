f2a247e24509ea00f681671db973469da3bffd9f
Tanveer Ansari <tanveeransari@hotmail.com>
2015-06-16 20:51:18 -0500

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix allows getting setting the matrix and its inverse
+## It caches the value in variable inv
 
-## Write a short comment describing this function
+## cacheSolve checks and returns cached value if it exists, else matrix inverse
 
+## inv variable is the cached inverse
+## makeCacheMatrix is a list containing functions to
+## get & set value of matrix, get & set value of inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<- function(y) {
+    x<<-y
+    inv<<-NULL
+  }
+  
+  get<-function() x
+  
+  setinv <- function(i) inv<<- i
+  
+  getinv <- function() inv
+  
+  list(get=get,set=set, getinv=getinv, setinv=setinv)
 }
 
 
-## Write a short comment describing this function
+## Check if inv already calculated using getinv()
+## If so return cached value, otherwise
+## solve for matrix inverse, set it and return it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
